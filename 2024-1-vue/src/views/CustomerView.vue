dc0fdf62eef0787daf72b3a02c8296e067f5a643
岩本　章宏 <iwamotoakihiro@iwamotoakihironoMacBook-Pro.local>
2024-02-08 06:56:12 +0900

    提出　１回目

---
+++ 2024-1-vue/src/views/CustomerView.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="customer-contener">
+    <div class="customer-information-title">
+      <h1 class="information-title-text">お客様情報入力</h1>
+    </div>
+    <div class="customer-wrapper">
+      <div class="customer-information">
+        <div v-for="field in fields" :key="field.id" class="import-box">
+          <h1 class="import-title-text">{{ field.label }}</h1>
+          <input :type="field.type" :name="field.name" :id="field.id" class="import-form">
+        </div>
+      </div>
+      <div class="payment-information">
+        <div class="payment-serect">
+          <div v-for="option in paymentOptions" :key="option.value">
+            <input type="radio" :id="option.value" :name="option.name" :value="option.value" v-model="selectedOption">
+            <label :for="option.value">{{ option.label }}</label>
+          </div>
+        </div>
+        <div v-if="selectedOption === 'bank'" class="category-contents">
+          <div class="bank-payment-information">
+            <div v-for="field in bankFields" :key="field.id" class="payment-form-box">
+              <h1 class="payment-title">{{ field.label }}</h1>
+              <input type="text" :name="field.name" :id="field.id" class="payment-form">
+              <div v-if="field.id === 1 " class="bank-type-serect">
+                <div v-for="bankType in bankTypes" :key="bankType.value" >
+                  <input type="radio" :id="bankType.value" :name="bankType.name" :value="bankType.value" :checked="bankType.checked">
+                  <label :for="bankType.value">{{ bankType.label }}</label>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div v-else-if="selectedOption === 'credit'" class="category-contents">
+          <div class="credit-payment-information">
+            <div v-for="field in creditFields" :key="field.id" class="payment-form-box">
+              <h1 class="payment-title">{{ field.label }}</h1>
+              <input type="text" :name="field.name" :id="field.id" class="payment-form">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="customer-next-link">
+      <button class="next-link-text">注文確認画面へ</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fields: [
+        { id: 1, label: "お名前", type: "text", name: "name" },
+        { id: 2, label: "フリガナ", type: "text", name: "kana" },
+        { id: 3, label: "メールアドレス", type: "email", name: "email" },
+        { id: 4, label: "電話番号", type: "tel", name: "tel" },
+        { id: 5, label: "郵便番号", type: "text", name: "tel" },
+        { id: 6, label: "都道府県", type: "text", name: "tel" },
+        { id: 7, label: "市町村", type: "text", name: "tel" },
+        { id: 8, label: "以下番地", type: "text", name: "tel" },
+        { id: 9, label: "マンション,アパート名", type: "text", name: "tel" },
+        { id: 10, label: "部屋番号", type: "text", name: "tel" }
+      ],
+      paymentOptions: [
+        { value: "bank", name: "paymentMethod", label: "口座" },
+        { value: "credit", name: "paymentMethod", label: "クレジット" }
+      ],
+      selectedOption: "bank" ,
+      bankTypes: [
+        { value: "normalBank", name: "bankType", label: "普通", checked: true },
+        { value: "currentBank", name: "bankType", label: "当座", checked: false }
+      ],
+      bankFields: [
+        { id: 1, label: "金融機関", name: "bankName" }, 
+        { id: 2, label: "口座番号", name: "bankNumber" },
+        { id: 3, label: "支店番号", name: "sateliteNumber" },
+        { id: 4, label: "氏名（カナ）", name: "bankUser" }
+      ],
+      creditFields: [
+        { id: 1, label: "カード番号", name: "creditNumber" }, 
+        { id: 2, label: "氏名", name: "creditUser" },
+        { id: 3, label: "有効期限", name: "creditExpire" },
+        { id: 4, label: "セキュリティーコード", name: "securityCord" }
+      ],
+    };
+  }
+};
+</script>
+<style>
+.customer-contener {
+  padding: 40px;
+}
+.customer-information-title {
+  padding: 40px 0;
+}
+.information-title-text {
+  font-size: 40px;
+  font-weight: bold;
+
+}
+.customer-wrapper {
+  display: flex;
+  justify-content: center;
+}
+.customer-information,.category-contents {
+  border: 1px solid #000;
+  justify-content: center;
+  padding: 40px;
+  width: 580px;
+}
+.category-contents {
+  margin-top: 40px;
+}
+.payment-information {
+  margin-left: 40px;
+}
+.payment-serect, .bank-type-serect {
+  display: flex;
+  justify-content: center;
+  font-size: 32px;
+}
+.import-box,
+.payment-form-box,
+.bank-type-serect {
+  margin-top: 36px;
+}
+.import-box:first-of-type,.payment-form-box:first-of-type {
+  margin-top: 0;
+}
+.import-form,.payment-form {
+  width: 100%;
+  height: 65px;
+  background-color: #d9d9d9;
+  border-radius: 40px;
+  margin-top: 20px;
+}
+</style>
---
+++ 2024-1-vueのコピー/src/views/CartView.vue
@@ -0,0 +1,271 @@
+<template>
+  <main>
+    <div class="cart-contener">
+      <div class="cart-title-box">
+        <h1 class="cart-title">カート 一覧</h1>
+      </div>
+      <section class="incart-item-contenner">
+        <div class="incart-item-wrapper">
+          <div v-for="(Incart_itemGroup, index) in Incart_itemGroups" :key="index" class="incart-item-row">
+            <div v-for="(item, idx) in Incart_itemGroup" :key="idx" class="incart-item-box">
+              <div class="incart-item-fot">
+                <img :src="item.imgUrl" alt="">
+              </div>
+              <div class="incart-item-text-box">
+                <div class="incart-item-title-box">
+                  <h2 class="incart-item-title">{{ item.title }}</h2>
+                  <div class="incart-item-favorite">
+                    <img src="../assets/clarity_favorite-line.png" alt="">
+                  </div>
+                </div>
+                <div class="incart-item-explanation">{{ item.description }}</div>
+                <div class="incart-item-count-box">
+                  <div class="incart-item-count">
+                    <button class="incart-item-count-button">
+                      <img src="../assets/uiw_minus-circle.png" alt="" class="incart-item-count-icon">
+                    </button>
+                    0
+                    <button class="incart-item-count-button">
+                      <img src="../assets/uiw_plus-circle.png" alt="">
+                    </button>
+                  </div>
+                  <div class="incart-dust-box">
+                    <img src="../assets/Vector.png" alt="" class="incart-item-count-icon">
+                  </div>
+                </div>
+                <div class="incart-item-price">{{ item.price }}円</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div class="cart-after-contener">
+        <div class="cart-total-box">
+          <div class="cart-total-items">
+            <div class="cart-items-count">購入点数</div>
+            <div class="cart-item-unit">00<span>個</span></div>
+          </div>
+          <div class="caet-total-price">
+            <div class="caet-price-count">合計</div>
+            <div class="cart-item-unit">00000<span>個</span></div>
+          </div>
+        </div>
+        <button class="cart-next-link-box">
+          <RouterLink to="/customer">
+            <div class="cart-next-link-text">
+              <p>お客様情報の入力</p>
+            </div>
+          </RouterLink>
+        </button>
+      </div>
+    </div>
+  </main>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { 
+          title: "アウター商品A", 
+          description: "アウター商品Aの説明テキスト", 
+          price: "12000",
+          imgUrl: "https://drive.google.com/uc?id=18YdbWdP7bawITaOy0kfbkHBrpl3zujCM/view?usp=drive_link"
+        },
+        { 
+          title: "アウター商品B", 
+          description: "アウター商品Bの説明テキスト", 
+          price: "14000",
+          imgUrl: "https://drive.google.com/uc?id=18YdbWdP7bawITaOy0kfbkHBrpl3zujCM/view?usp=drive_link"
+        },
+        { 
+          title: "アウター商品C", 
+          description: "アウター商品Cの説明テキスト", 
+          price: "11000",
+          imgUrl: "https://drive.google.com/uc?id=18YdbWdP7bawITaOy0kfbkHBrpl3zujCM/view?usp=drive_link"
+        },
+        { 
+          title: "アウター商品D", 
+          description: "アウター商品Dの説明テキスト", 
+          price: "15000",
+          imgUrl: "https://drive.google.com/uc?id=18YdbWdP7bawITaOy0kfbkHBrpl3zujCM/view?usp=drive_link"
+        },
+        { 
+          title: "アウター商品E", 
+          description: "アウター商品Eの説明テキスト", 
+          price: "12000",
+          imgUrl: "https://drive.google.com/uc?id=18YdbWdP7bawITaOy0kfbkHBrpl3zujCM/view?usp=drive_link"
+        },
+        { 
+          title: "アウター商品F", 
+          description: "アウター商品Fの説明テキスト", 
+          price: "11000",
+          imgUrl: "https://drive.google.com/uc?id=18YdbWdP7bawITaOy0kfbkHBrpl3zujCM/view?usp=drive_link"
+        }
+      ]
+    };
+  },
+  computed: {
+    Incart_itemGroups() {
+      const groups = [];
+      for (let i = 0; i < this.items.length; i += 3) {
+        groups.push(this.items.slice(i, i + 3));
+      }
+      return groups;
+    }
+  }
+}
+</script>
+<style>
+.cart-contener {
+  margin: 32px 40px;
+}
+.incart-item-contenner {
+  display: flex;
+  padding: 32px 0px;
+  flex-direction: column;
+}
+
+.incart-item-title {
+  display: flex;
+  align-items: flex-start;
+  align-self: stretch;
+  font-size: 40px;
+}
+
+.incart-item-wrapper {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+
+.incart-item-row {
+  display: flex;
+}
+
+.incart-item-box {
+  display: flex;
+  padding: 16px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  background: #D8D8D8;
+  width: 320px;
+  margin-bottom: 20px;
+  font-size: 16px;
+  margin-left: auto;
+}
+.incart-item-box:first-child {
+  margin-left: 0;
+}
+.incart-item-fot {
+  width: 288px;
+  aspect-ratio: 288/192;
+}
+
+.incart-item-title-box {
+  display: flex;
+}
+
+.incart-item-title {
+  font-size: 20px;
+}
+
+.incart-item-favorite {
+  margin: 0 0 0 auto;
+}
+
+.incart-item-text-box {
+  padding: 20px 0px;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 32px;
+  width: 256px;
+}
+
+.incart-item-explanation {
+  margin-top: 32px;
+}
+
+.incart-item-count-box {
+  display: flex;
+  width: 157px;
+  padding: 4px 18px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 20px;
+  background: #FFF;
+  margin: 32px 0 0 auto;
+}
+
+.incart-item-count {
+  display: flex;
+  gap: 20px;
+  background: none;
+  border: none;
+  cursor: pointer;
+}
+
+.incart-item-count-button {
+  background: none;
+  border: none;
+}
+
+.incart-item-count-icon {
+  width: auto;
+}
+
+.incart-dust-box {
+  width: auto;
+  margin-left: 20px;
+  cursor: pointer;
+}
+
+.incart-item-price {
+  margin-top: 12px;
+  text-align: right;
+}
+
+.incart-top-item-link {
+  color: #000;
+  background-color: #CCCCCC;
+  display: flex;
+  width: fit-content;
+  font-size: 40px;
+  padding: 8px 16px;
+  align-items: center;
+  justify-content: center;
+  align-self: stretch;
+  margin: 0 24px 0 auto;
+}
+.cart-after-contener {
+  text-align: right;
+}
+.cart-total-box {
+  width: 320px;
+  margin: 0 0 0 auto;
+}
+.cart-total-items {
+  display: flex;
+  font-size: 32px;
+}
+
+.caet-total-price {
+  display: flex;
+  font-size: 36px;
+  margin-top: 32px;
+}
+.cart-item-unit {
+  margin: 0 0 0 auto;
+}
+.cart-next-link-box {
+  background-color: #d9d9d9;
+  border-radius: 40px;
+  border: 5px solid #000;
+  height: 96px;
+  padding: 0 36px;
+  font-size: 36px;
+  margin-top: 20px;
+}
+</style>

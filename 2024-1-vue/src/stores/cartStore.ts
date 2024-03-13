346c8ba53789cc0ae169fd032ec6fe9ce1834f4c
shuto <1023shuto@gmail.com>
2024-03-14 03:19:17 +0900

    確認してね

--- 2024-1-vue/src/stores/cartStore.ts
+++ 2024-1-vue/src/stores/cartStore.ts
@@ -7,50 +7,54 @@ export const useCartStore = defineStore('cartStore', {
   state: () => ({
     // カート内の商品情報を格納する配列
     cartItems: [],
+    cartCounts: [],
   }),
 
   // アクションを定義
   actions: {
     // カートに商品を追加するアクション
     addToCart({ productId, product }) {
-      const quantity = this.getCounter(productId);
-      if (quantity > 0) {
-        const cartItem = this.cartItems.find(item => item.product.product_id === productId);
-        if (cartItem) {
-          cartItem.quantity += quantity;
-        } else {
-          // カート内の商品リストに新しい商品を追加
-          this.cartItems.push({ product, quantity });
-        }
+      const cartItem = this.cartItems.find(item => item.product_id === productId);
+      const cartCount = this.cartCounts.find(item => item.product_id === productId);
+      if (cartItem) {
+        cartItem.quantity += cartCount.quantity;
+      } else {
+        // カート内の商品リストに新しい商品を追加
+        this.cartItems.push({ ...product, quantity: cartCount.quantity });
       }
     },
     
     // 商品の数量を増やすアクション
     incrementCounter(productId) {
-      const cartItem = this.cartItems.find(item => item.product.productId === productId);
-      if (cartItem) {
+      const cartItem = this.cartItems.find(item => item.product_id === productId);
+      const cartCount = this.cartCounts.find(item => item.product_id === productId);
+      if (cartCount) {
+        cartCount.quantity++;
         cartItem.quantity++;
       } else {
         // カートに該当商品がない場合、新しい商品を追加
-        this.cartItems.push({ product: { productId }, quantity: 1 });
+        this.cartCounts.push({ product_id: productId, quantity: 1 });
       }
     },
 
     // 商品の数量を減らすアクション
     decrementCounter(productId) {
-      const cartItem = this.cartItems.find(item => item.product.productId === productId);
+      const cartItem = this.cartItems.find(item => item.product_id === productId);
+      const cartCount = this.cartCounts.find(item => item.product_id === productId);
       if (cartItem && cartItem.quantity > 1) {
         cartItem.quantity--;
+        cartCount.quantity--;
       } else {
         // カート内の数量が1以下の場合、商品を削除
-        this.cartItems = this.cartItems.filter(item => item.product.productId !== productId);
+        this.cartItems = this.cartItems.filter(item => item.product_id !== productId);
+        this.cartCounts = this.cartCounts.filter(item => item.product_id !== productId);
       }
     },
 
     // 特定の商品の数量を取得するアクション
     getCounter(productId) {
-      const cartItem = this.cartItems.find(item => item.product.productId === productId);
-      return cartItem ? cartItem.quantity : 0;
+      const cartCount = this.cartCounts.find(item => item.product_id === productId);
+      return cartCount ? cartCount.quantity : 0;
     },
 
     // 特定の商品をカートから削除するアクション
